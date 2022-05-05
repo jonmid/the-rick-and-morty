c1f7b4dad7dba0923b2e88a8110e1224716a34a7
Jhonatan Mideros <10jonmid@gmail.com>
2022-05-05 14:40:39 -0500

    [add] se crea componente de busqueda de personaje

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import { Character } from './components/Character'
-import { ListCharacter } from './components/ListCharacter'
+import { Search } from './components/Search'
 
 function App() {
   return (
     <div>
-      <h1 className='text-center text-3xl text-teal-600 font-bold'>The Rick and Morty</h1>
+      <h1 className='text-center text-3xl text-blue-600 font-bold'>The Rick and Morty</h1>
+      <Search />
     </div>
   )
 }

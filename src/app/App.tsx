0305a8caecf3a9881babf2c53af865b800ace768
Jhonatan Mideros <10jonmid@gmail.com>
2022-05-05 15:30:44 -0500

    [add] creacion de componente button que nos muestra todos los favoritos seleccionados

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import { Empty } from './components/Empty'
+import { ButtonFavorite } from './components/ButtonFavorite'
 
 function App() {
   return (
     <div>
       <h1 className='text-center text-3xl text-app-600 font-bold'>The Rick and Morty</h1>
-      <Empty title='Pagina' message='No se encontro datos para mostrar' />
+      <ButtonFavorite />
     </div>
   )
 }

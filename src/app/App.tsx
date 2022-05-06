f3545a6a2cd9c35c8f80b05f8c56eb779e7df549
Jhonatan Mideros <10jonmid@gmail.com>
2022-05-06 09:12:05 -0500

    [update] se actualiza App.tsx para mostrar el desarrollador de la app

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -13,6 +13,16 @@ function App() {
     <Layout>
       <Logo />
 
+      <div className='flex items-center justify-center my-10'>
+        <span className='ml-2 inline-flex items-center text-lg font-thin text-center text-app-600'>
+          Created with
+          <svg fill='currentColor' className='mx-1 w-3 h-3 text-red-500' viewBox='0 0 16 16'>
+            <path d='M4 1c2.21 0 4 1.755 4 3.92C8 2.755 9.79 1 12 1s4 1.755 4 3.92c0 3.263-3.234 4.414-7.608 9.608a.513.513 0 0 1-.784 0C3.234 9.334 0 8.183 0 4.92 0 2.755 1.79 1 4 1z' />
+          </svg>
+          by Jhonatan Mideros
+        </span>
+      </div>
+
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/bookmarks' element={<Bookmarks />} />

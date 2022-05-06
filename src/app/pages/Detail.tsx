a155ad4e44b9a55b7476586cd92a35d9168af6e6
Jhonatan Mideros <10jonmid@gmail.com>
2022-05-06 07:19:00 -0500

    [update] se actualiza pagina de detail para cuando no se selecciona personaje

--- src/app/pages/Detail.tsx
+++ src/app/pages/Detail.tsx
@@ -3,13 +3,18 @@ import { useSelector } from 'react-redux'
 
 import { RootState } from './../store/store'
 import { HeroDetail } from '../components/HeroDetail'
+import { Empty } from '../components/Empty'
+import { Breadcrumbs } from '../components/Breadcrumbs'
 
 const Detail = () => {
   const character = useSelector((state: RootState) => state.characterReducer.character)
 
+  console.log(character)
+
   return (
     <>
-      <HeroDetail character={character} />
+      <Breadcrumbs title='Detail' />
+      {Object.keys(character).length !== 0 ? <HeroDetail character={character} /> : <Empty title='No character selection' message='return to home to select a character' />}
     </>
   )
 }

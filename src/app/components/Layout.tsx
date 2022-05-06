46c20edb0f661ae66dccad94a5e93aeaba161121
Jhonatan Mideros <10jonmid@gmail.com>
2022-05-06 08:58:00 -0500

    [update] se actualiza componente layout ajustando los estilos de padding

--- src/app/components/Layout.tsx
+++ src/app/components/Layout.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Props } from './../utilities/interfaces'
 
 const Layout = ({ children }: Props) => {
-  return <div className='container mx-auto flex flex-col px-4 md:px-0'>{children}</div>
+  return <div className='container mx-auto flex flex-col px-4'>{children}</div>
 }
 
 export { Layout }
